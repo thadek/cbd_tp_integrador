500de624c20bab535a0c95ca6b089fbd186417c0
alejandraob <alejandraboisier@hotmail.com>
2023-06-08 19:56:27 -0300

    ejercicio finalizados probar- se actualizaron datos en inserciondedatos

--- DML/Ejercicio_4/ejercicio4.sql
+++ DML/Ejercicio_4/ejercicio4.sql
@@ -11,8 +11,18 @@ WHERE i.fechaInscripcion = DATE_SUB(c.Fecha, INTERVAL 5 DAY);
 SELECT `nroAula`, `nroAsiento`, `fila` FROM `asiento` INNER JOIN aula ON (aula.numero= asiento.nroAula AND aula.tipo = "GRANDE") 
 WHERE NOT EXISTS (SELECT * FROM inscripcion WHERE inscripcion.NroAsiento = asiento.nroAsiento);
 
+--c) Listar la cantidad de cursos por cada tema principal 
+--en lo que va de este año 2022. Listar NombreTema, 
+--contenido y cantidad
+SELECT curso.NombreTemaPrincipal, temario.contenido, COUNT(curso.NombreTemaPrincipal) AS cantidad 
+FROM curso 
+INNER JOIN temario ON (curso.NombreTemaPrincipal = temario.Nombre) 
+WHERE curso.Fecha BETWEEN '2022-01-01' AND '2022-12-31' 
+GROUP BY curso.NombreTemaPrincipal, temario.contenido;
 
+--d) Listar la cantidad de inscripciones realizadas por fecha que correspondan a cursos del mes de Abril de 2022 
+--y además el tema secundario debe contener ‘Base de datos’. Listar fecha de inscripcion y cantidad. Utilice el 
+--operador IN para determinar el tema secundario.
 
-
-
+SELECT `inscripcion`.`fechaInscripcion`, COUNT(`inscripcion`.idCurso) as cantidad FROM `inscripcion` INNER JOIN curso on(inscripcion.idCurso=curso.IdCurso) WHERE curso.NombreTemaSecundario in (SELECT nombre FROM temario where curso.NombreTemaSecundario='Base de Datos') AND curso.Fecha BETWEEN '2022-04-01' AND '2022-04-30' GROUP by `inscripcion`.idCurso;
 
